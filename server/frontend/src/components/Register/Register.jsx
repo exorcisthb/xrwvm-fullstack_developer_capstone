d52508c9e124bff8faf7bcc1395ecaaded5d81b1
HaiiiiHungggg <exorcisthb@gmail.com>
2026-04-08 19:35:31 +0700

    Wrap registration inputs in a form element

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -14,12 +14,14 @@ function Register() {
   return (
     <div>
       <h1>Sign Up</h1>
-      <input type="text" placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
-      <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} />
-      <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
-      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleRegister}>Register</button>
+      <form>
+        <input type="text" placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
+        <input type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} />
+        <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleRegister}>Register</button>
+      </form>
     </div>
   );
 }
